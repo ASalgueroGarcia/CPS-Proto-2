5147662ce2e3facf35916a410b81e1b129d0c112
ASalgueroGarcia <antonio.pedro.salguero@gmail.com>
2026-03-25 12:13:04 +0100

    Remade Map

--- Assets/_AntonioAssets/_Scripts/Node.cs
+++ Assets/_AntonioAssets/_Scripts/Node.cs
@@ -5,6 +5,11 @@ public class Node : MonoBehaviour
 {
     [Header("Node Settings")] 
     [SerializeField] private NodeTypeEnum type;
+
+    [Header("References")]
+    [SerializeField] private LineBetweenObjects line;
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private bool generateType;
     
     private readonly List<Node> _parentNodes = new List<Node>();
     private readonly List<Node> _childNodes = new List<Node>();
@@ -14,24 +19,18 @@ public class Node : MonoBehaviour
     private bool _isActive = false;
     private bool _isStartingNode = false;
 
-    private LineRenderer _lr;
+    private LineBetweenObjects _lbo;
+    private readonly HashSet<Node> _connectedChildren = new HashSet<Node>();
 
     private void Awake()
     {
-        _lr = gameObject.AddComponent<LineRenderer>();
-        _lr.startColor = Color.red;
-        _lr.endColor = Color.green;
-        _lr.startWidth = 0.2f;
-        _lr.endWidth = 0.2f;
-        _lr.enabled = false;
-        
-        GetComponent<NodeBehaviour>().SetNode(this);
-        
+        canvas = FindObjectOfType<Canvas>();
         GenerateNodeType();
     }
 
     private void GenerateNodeType()
     {
+        if (!generateType) return;
         type = Random.Range(_min, _max + 1) <= 75 ? NodeTypeEnum.Combat : NodeTypeEnum.Merchant;
 
         //Debug.Log(this.gameObject.name + type);
@@ -82,18 +81,23 @@ public class Node : MonoBehaviour
 
         if (!_isActive || _childNodes == null) return;
 
-        _lr.positionCount = _childNodes.Count * 2;
-        _lr.enabled = true;
-        
-        for (var i = 0; i < _childNodes.Count; i++)
+        foreach (var child in _childNodes)
         {
-            _lr.SetPosition(i * 2, transform.position);
-            _lr.SetPosition(i * 2 + 1, _childNodes[i].transform.position);
+            if (!child._isActive || _connectedChildren.Contains(child)) continue;
+        
+            var connectingLine = Instantiate(line, canvas.transform);
+            connectingLine.SetObjects(this.gameObject, child.gameObject);
+            _connectedChildren.Add(child);
         }
     }
 
     public bool HasNode(Node node)
     {
         return _childNodes.Contains(node);
+    }
+
+    public void SetType(NodeTypeEnum nodeType)
+    {
+        type = nodeType;
     }
 }
--- Assets/_AntonioAssets/_Scripts/NodeBehaviour.cs
+++ Assets/_AntonioAssets/_Scripts/NodeBehaviour.cs
@@ -1,43 +1,14 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 
 public class NodeBehaviour : MonoBehaviour
 {
-    [Header("Scene Management")] 
-    [SerializeField] private SceneController sceneController;
-
-    private Node _node;
-    
-    private float _min;
-    
-    private void Awake()
+    public void LoadLevel()
     {
-        sceneController = FindObjectOfType<SceneController>();
+        var sceneController = SceneController.Instance;
+        var node = GetComponentInParent<Node>();
 
-        if (sceneController == null)
-            Debug.LogError("No SceneController found in scene!", this);
-    }
-    
-    private void Update()
-    {
-        if (Mouse.current.leftButton.wasPressedThisFrame) MousePressed();
-    }
+        Debug.Log($"Button object: {gameObject.name}, Parent: {transform.parent?.name}, Node: {node?.gameObject.name}, Type: {node?.GetNodeType()}", this);
 
-    public void SetNode(Node node)
-    {
-        _node = node;
-    }
-
-    private void MousePressed()
-    {
-        var ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-        Physics.Raycast(ray, out var hit);
-
-        if (!hit.transform || hit.transform.gameObject != gameObject) return;
-
-        Debug.Log($"sceneController: {sceneController}, _node: {_node}", this);
-
-        sceneController.LoadLevel(_node.GetNodeType());
+        if (node) sceneController.LoadLevel(node.GetNodeType());
     }
 }
