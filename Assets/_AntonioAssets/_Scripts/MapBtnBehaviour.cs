8301e41d2e6e31f2263816d38ff884a9436e7351
ASalgueroGarcia <antonio.pedro.salguero@gmail.com>
2026-03-28 17:48:05 +0100

    Update MapBtnLogic

--- Assets/_AntonioAssets/_Scripts/MapBtnBehaviour.cs
+++ Assets/_AntonioAssets/_Scripts/MapBtnBehaviour.cs
@@ -4,17 +4,24 @@ public class MapBtnBehaviour : MonoBehaviour
 {
     [Header("Pause Menu Settings")]
     [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject eolCanvas;
     
     public void ReturnToMap()
     {
         if (SceneController.Instance)
         {
             if (pauseMenu.activeSelf) pauseMenu.SetActive(!pauseMenu.activeSelf);
+            if (eolCanvas.activeSelf) ToggleEoLCanvas();
             SceneController.Instance.UnloadLevel();
         }
         else
         {
             Debug.LogError("SceneController instance is null! Cannot return to map.");
         }
+    }
+
+    public void ToggleEoLCanvas()
+    {
+        eolCanvas.SetActive(!eolCanvas.activeSelf);
     }
 }
