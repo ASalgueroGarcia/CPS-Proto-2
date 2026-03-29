4e350965c1caa15e58c6a906a8cd276f226486e8
ASalgueroGarcia <antonio.pedro.salguero@gmail.com>
2026-03-29 22:51:15 +0200

    Rename PathGenerator to MapBehaviour; add nodeIndex
    Refactor map generation: renamed PathGenerator.cs to MapBehaviour.cs and updated the class to manage node indices and completion flow. Added nodeIndex fields to multiple node prefabs and the map scene, set Node tag on node button prefabs, and removed some unused serialized fields. Introduced SetActiveNodes (assigns node indices), CompletedNode, and SetCurrentNodeIndex to handle node activation and progression. Also moved/renamed the _DimaAsstets folder into Assets/_AntonioAssets/_DimaAsstets and updated the map scene to reference the new MapBehaviour and SceneController components.

--- Assets/_AntonioAssets/_Scripts/NodeBehaviour.cs
+++ Assets/_AntonioAssets/_Scripts/NodeBehaviour.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class NodeBehaviour : MonoBehaviour
 {
+    private MapBehaviour _mapBehaviour;
+
+    private void Start()
+    {
+        _mapBehaviour = FindFirstObjectByType<MapBehaviour>();
+    }
+
     public void LoadLevel()
     {
         var sceneController = SceneController.Instance;
         var node = GetComponentInParent<Node>();
+        
+        /*
+        Debug.Log($"Button object: {gameObject.name}, Parent: {transform.parent?.name}, " +
+                  $"Node: {node?.gameObject.name}, Type: {node?.GetNodeType()}, " +
+                  $"Index: {transform.parent?.GetComponent<Node>().GetNodeIndex()}", this);
+        */
 
-        Debug.Log($"Button object: {gameObject.name}, Parent: {transform.parent?.name}, Node: {node?.gameObject.name}, Type: {node?.GetNodeType()}", this);
+        if (!node) return;
 
-        if (node) sceneController.LoadLevel(node.GetNodeType());
+        if (transform.parent) _mapBehaviour.SetCurrentNodeIndex(transform.parent.GetComponent<Node>().GetNodeIndex());
+        sceneController.LoadLevel(node.GetNodeType());
     }
-    
 }
