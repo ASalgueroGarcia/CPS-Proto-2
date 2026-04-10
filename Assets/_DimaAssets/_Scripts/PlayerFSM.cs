df1fc78ee0419e05401c792982bdf3cf5e5271d4
AnaLopezLanda <ana.lopez@live.u-tad.com>
2026-04-10 09:43:29 +0200

    Knockback and commment

--- Assets/_DimaAssets/_Scripts/PlayerFSM.cs
+++ Assets/_DimaAssets/_Scripts/PlayerFSM.cs
@@ -41,7 +41,7 @@ public class PlayerFSM : MonoBehaviour
     public bool wasHit = false;
     public float attackRange = 2.0f;
     public float specialRange = 5.0f;
-    [SerializeField] private float knockBackForce;
+    [SerializeField] private float knockBackForce = 7;
 
     [Header("Combo Settings")] 
     public int comboStep = 0;
@@ -434,6 +434,8 @@ public class PlayerFSM : MonoBehaviour
 
         // Damage + Knockback
         Collider[] hitEnemies = Physics.OverlapSphere(transform.position, specialRange, enemyLayer);
+        //This line is fucking up the collisions making it so the player doesnt hit the enemies everytime
+        //This could be fixed by attaching the damage of the enemies to the collider on the swords
         foreach (Collider enemy in hitEnemies)
         {
             Health h = enemy.GetComponent<Health>();
